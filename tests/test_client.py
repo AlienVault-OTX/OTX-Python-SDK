70053bbdb1af21d8f1ecc36cce3aab866d6128dc
Bill Smartt <bsmartt@alienvault.com>
2016-05-01 18:12:25 -0700

    Unit tests for existing endpoints.  coverage up to 89%.

--- tests/test_client.py
+++ tests/test_client.py
@@ -4,6 +4,7 @@ import dateutil.parser
 
 from utils import generate_rand_string
 from OTXv2 import OTXv2, InvalidAPIKey
+from IndicatorTypes import IPv4
 
 ALIEN_API_APIKEY = "48db25670b590ae34850cb13e25397e1e6cad56f2c4f7bdae75a1121dc76bdd0"
 
@@ -53,15 +54,66 @@ class TestSubscriptions(TestOTXv2):
             self.assertTrue(pulse.get('name', None))
 
     def test_getsince(self):
-        year_ago_dt = (datetime.datetime.now() - datetime.timedelta(days=90))
-        year_ago_timestamp = year_ago_dt.isoformat()
-        pulses = self.otx.getsince(year_ago_timestamp, limit=9999)
+        three_months_dt = (datetime.datetime.now() - datetime.timedelta(days=90))
+        three_months_timestamp = three_months_dt.isoformat()
+        pulses = self.otx.getsince(three_months_timestamp, limit=9999)
         for pulse in pulses:
             pulse_modified = pulse.get('modified', None)
             self.assertIsNotNone(pulse_modified)
             pulse_modified_dt = dateutil.parser.parse(pulse_modified)
-            self.assertGreaterEqual(pulse_modified_dt, year_ago_dt)
+            self.assertGreaterEqual(pulse_modified_dt, three_months_dt)
 
+    def test_getsince_iter(self):
+        three_months_dt = (datetime.datetime.now() - datetime.timedelta(days=90))
+        three_months_timestamp = three_months_dt.isoformat()
+        pulse_gen = self.otx.getsince_iter(three_months_timestamp, limit=9999)
+        self.assertIsNotNone(pulse_gen)
+        for pulse in pulse_gen:
+            self.assertTrue(pulse.get('name', None))
+            pulse_modified = pulse.get('modified', None)
+            self.assertIsNotNone(pulse_modified)
+            pulse_modified_dt = dateutil.parser.parse(pulse_modified)
+            self.assertGreaterEqual(pulse_modified_dt, three_months_dt)
+
+
+class TestEvents(TestOTXv2):
+    def setUp(self, **kwargs):
+        super(TestEvents, self).setUp(**{'api_key': ALIEN_API_APIKEY})
+
+    def test_getevents_since(self):
+        three_months_dt = (datetime.datetime.now() - datetime.timedelta(days=90))
+        three_months_timestamp = three_months_dt.isoformat()
+        events = self.otx.getevents_since(three_months_timestamp, limit=9999)
+        self.assertIsNotNone(events)
+        self.assertTrue(len(events) > 0)
+        most_recent = events[0]
+        print "Next event: {0}".format(most_recent.keys())
+        self.assertIsNotNone(most_recent.get('action', None))
+        self.assertIsNotNone(most_recent.get('created', None))
+        self.assertIsNotNone(most_recent.get('id', None))
+
+
+class TestIndicatorTypes(TestOTXv2):
+    def setUp(self, **kwargs):
+        super(TestIndicatorTypes, self).setUp(**{'api_key': ALIEN_API_APIKEY})
+
+    def test_get_all_indicators(self):
+        indicator_gen = self.otx.get_all_indicators()
+        for indicator in indicator_gen:
+            self.assertIsNotNone(indicator)
+            self.assertIsNotNone(indicator.get('type', None))
+            self.assertIsNotNone(indicator.get('indicator', None))
+            self.assertIsNotNone(indicator.get('description', None))
+
+    def test_get_all_ipv4_indicators(self):
+        ipv4_type_list = [IPv4]
+        ipv4_indicator_gen = self.otx.get_all_indicators(indicator_types=ipv4_type_list)
+        for indicator in ipv4_indicator_gen:
+            self.assertIsNotNone(indicator)
+            self.assertIsNotNone(indicator.get('type', None))
+            self.assertIsNotNone(indicator.get('indicator', None))
+            self.assertIsNotNone(indicator.get('description', None))
+            self.assertTrue(indicator.get('type', '') == IPv4.name)
 
 if __name__ == '__main__':
     unittest.main()
