2f9c65d247516d389c3952e9c54980f73a896e49
Chris Doman <cdoman@alienvault.com>
2019-04-02 16:07:20 +0100

    Commit version number

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from distutils.core import setup
 
 setup(
     name='OTXv2',
-    version='1.1',
+    version='1.41',
     description='AlienVault OTX API',
     author='AlienVault Team',
     author_email='otx@alienvault.com',
     url='https://github.com/AlienVault-Labs/OTX-Python-SDK',
-    download_url='https://github.com/AlienVault-Labs/OTX-Python-SDK/tarball/1.1.0',
+    download_url='https://github.com/AlienVault-Labs/OTX-Python-SDK/tarball/1.41',
     py_modules=['OTXv2', 'IndicatorTypes','patch_pulse'],
     install_requires=['simplejson', 'requests', 'python-dateutil', 'pytz']
 )
