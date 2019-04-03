5b8c818538464d062583b7c359c0be0e1ded0b39
Rusty Brooks <rbrooks@alienvault.com>
2019-04-03 17:46:01 -0500

    Update to version 1.5

--- OTXv2.py
+++ OTXv2.py
@@ -72,7 +72,7 @@ class OTXv2(object):
         self.request_session = None
         self.headers = {
             'X-OTX-API-KEY': self.key,
-            'User-Agent': user_agent or 'OTX Python {}/1.1'.format(project),
+            'User-Agent': user_agent or 'OTX Python {}/1.5'.format(project),
             'Content-Type': 'application/json'
         }
 
